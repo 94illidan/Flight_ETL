a4bb00d151b9ed1912f15dedffa69c714123ddc7
94illidan <51425276+94illidan@users.noreply.github.com>
2025-06-04 16:45:46 +0000

    delete file test

--- spark/scripts/test_spark_read.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("TestReadCSV").getOrCreate()
-
-df = spark.read.csv("/opt/spark/archivos/flights.csv", header=True, inferSchema=True)
-
-df.show(5)
-
-spark.stop()
